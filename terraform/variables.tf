02c922e4d2d1505339962858475f23980b6f3644
Dineshkumar-r <50138671+Dineshkumar-r@users.noreply.github.com>
2026-09-13 12:17:24 +0000

    fixed variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,29 @@
 variable "region" {
-    description = "GCP region"
-    type        = string
-    default     = "us-central1"
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
 }
 
 variable "project" {
-    description = "gcp project id"
-    type        = string
-    default     = "project-12dfcdc9-6860-459d-99f"
+  description = "GCP project ID"
+  type        = string
+  default     = "project-12dfcdc9-6860-459d-99f"
 }
 
 variable "environment" {
-    description = "gcp environment"
-    type        = string
-    default     = "production"
+  description = "GCP environment"
+  type        = string
+  default     = "production"
 }
+
 variable "vpc_cidr" {
-    description = "gcp vpc cidr creation"
-    type        = string
-    default     = "10.0.0.0/16"
+  description = "VPC CIDR range"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "cluster_name" {
-    description = "gke cluster name"
-    type        = string
-    default     = "jerney-gke"
+  description = "GKE cluster name"
+  type        = string
+  default     = "jerney-gke"
 }
