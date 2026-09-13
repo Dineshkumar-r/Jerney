8bc8182c740f18a145df0225d5f02f0d4aea145c
Dineshkumar-r <50138671+Dineshkumar-r@users.noreply.github.com>
2026-09-13 12:00:33 +0000

    fixed main.tf and wrote outputs.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,27 @@
 resource "google_compute_network" "vpc" {
   name = "jerney-vpc"
 
-  auto_create_subnetworks  = false
-  enable_ula_internal_ipv6 = true
+  auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "sub1" {
-  name = "jerney-vpc"
+  name = "jerney-subnet"
 
-  ip_cidr_range = var.vpc-cidr
+  ip_cidr_range = var.vpc_cidr
   region        = var.region
+
+  network = google_compute_network.vpc.id
+}
 
 resource "google_container_cluster" "cluster" {
   name = var.cluster_name
 
-  location                 = var.region
-  enable_autopilot         = true
-  enable_l4_ilb_subsetting = true
+  location = var.region
+
+  enable_autopilot = true
 
   network    = google_compute_network.vpc.id
   subnetwork = google_compute_subnetwork.sub1.id
-  }
+
+  deletion_protection = false
+}
